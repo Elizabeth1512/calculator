c6d5961d9731b54e6290547357cf2bb3940800bd
Elizaveta Romanova <elizaveta.romanova.1512@gmail.com>
2017-05-16 19:08:44 +0300

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -7,5 +7,68 @@ from tkinter import ttk
 root = Tk()
 root.title("Calculator")
 
-root.mainloop()
+#calculator logic
+def calc(key):
+    global memory
+    if key == "=":
 
+#4tob nelzja bqlo vvezti bulkvq(isklju4aem napisanie bukv)
+           strl = "-+0123456789.*/"
+           if calc_entry.get()[0] not in strl:
+               calc_entry.insert(END, "Pervqj simvol ne chislo")
+                messagebox.showerror("Error!", "Vq vvlei ne 4islo!")
+
+#s4et
+           try:
+		result = eval(calc_entry.get())
+		calc_entry.insert (END, "=" + str(result))
+	   except:
+		calc_entry.insert (END, "Error!")
+		messagebox.showerror("Error!", "Preverj pravilnostj dannqh")
+#o4istitj pole
+      elif key == "C":
+	   calc_entry.delete(0, END)
+#smena +-
+      elif key == "-+":
+	   if "=" in  calc_entry.get():
+	         calc_entry.delete(0, END)
+           try:
+		if calc_entry.get()[0] == "-":
+		       calc_entry.delete(0)
+		else:
+			calc_entry.insert(0, "-")
+	    except IndexError:
+		pass
+       else:
+	   if "=" in calc_entry.get():
+		calc_entry.delete(0, END)
+	   calc_entry.insert(END, key)     
+
+
+
+#screate a key
+bttn_list = [
+
+    "7", "8", "9", "+", "-"
+    "4", "5", "6", "*", "/"
+    "1", "2", "3", "-/+", "="
+    "0", ".", "?", "", ""
+]
+r = 1
+c = 0
+
+
+for i in bttn_list:
+    rel = ""
+   cmd =lambda x=i: calc(x)
+    ttk.Button(root, text=i, command=cmd).grid(row=r, column=c)
+    c += 1
+    if c>4:
+        c=0
+        r += 1
+
+calc_entry = Entry(root, width=33)
+calc_entry.grid(row=0,column=0, columnspan=5)
+
+
+root.mainloop()
