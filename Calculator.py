82fed99608f4da43a15c81b47650bc70e0c5f0e1
Elizaveta Romanova <elizaveta.romanova.1512@gmail.com>
2017-05-16 15:44:59 +0300

    Add files via upload
    I started:
    *i created a window

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+#That were the beautiful key
+from tkinter import ttk
+
+#create a window
+root = Tk()
+root.title("Calculator")
+
+root.mainloop()
+
