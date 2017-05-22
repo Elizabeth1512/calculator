093eabea1fd4791512ab1fd5adabc7c3a7c620fe
Elizaveta <elizaveta.romanova.1512@gmail.com>
2017-05-22 19:44:23 +0300

    rt

--- Calculator.py
+++ Calculator.py
@@ -9,33 +9,26 @@ root.title("Calculator")
 
 # calculator logic
 def calc(key):
-    print(key)
-global memory
-str1 = "-+0123456789.*/"
-
-if calc_entry.get()[0] not in str1:
-        calc_entry.insert(END, "The first symbol was not a number")
-        messagebox.showerror("Error!", "You did not enter a number!")
-        # score
-elif key == "C":
-        calc_entry.delete(0, END)
-    # changing +-
-
-elif key == "-+":
-
-else:
-
-    try:
+    global memory
+    if key == "=":
+        str1 = "-+0123456789.*/"
+        if calc_entry.get()[0] not in str1:
+            calc_entry.insert(END, "The first symbol was not a number")
+            messagebox.showerror("Error!", "You did not enter a number!")
+# score
+        try:
             result = eval(calc_entry.get())
             calc_entry.insert(END, "=" + str(result))
-    except:
+        except:
             calc_entry.insert(END, "Error!")
             messagebox.showerror("Error!", "Control your data")
-    # clear field
-
-
-if "=" in calc_entry.get():
+# clear field
+    elif key == "C":
         calc_entry.delete(0, END)
+ #   changing +-
+    elif key == "-/+":
+        if "=" in calc_entry.get():
+            calc_entry.delete(0, END)
         try:
             if calc_entry.get()[0] == "-":
                 calc_entry.delete(0)
@@ -43,25 +36,25 @@ if "=" in calc_entry.get():
                 calc_entry.insert(0, "-")
         except IndexError:
             pass
-        else:
-                if "=" in calc_entry.get():
-                    calc_entry.delete(0, END)
-                    calc_entry.insert(END, key)
+    else:
+        if "=" in calc_entry.get():
+            calc_entry.delete(0, END)
+            calc_entry.insert(END, key)
 
 # create a key
-bttn_list = [
+bttn_list = {
     "7", "8", "9", "+", "-",
     "4", "5", "6", "*", "/",
     "1", "2", "3", "-/+", "=",
     "0", ".", "c", "", ""
-]
+}
 r = 1
 c = 0
 
 
 for i in bttn_list:
     rel = ""
-    cmd =lambda x=i: calc(x)
+    cmd=lambda x=i: calc(x)
     ttk.Button(root, text=i, command=cmd).grid(row=r, column=c)
     c += 1
     if c > 4:
